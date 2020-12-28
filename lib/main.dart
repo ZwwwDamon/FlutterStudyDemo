1a39db424e3a8dc52b490b5250998491460d178f
zhenwen <zhenwen@mgtv.com>
2020-12-28 21:34:18 +0800

    路由跳转，待完善

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'first_app/counter_demo.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -11,43 +13,5 @@ class MyApp extends StatelessWidget {
         title: "FlutterStudyDemo",
         theme: ThemeData(primarySwatch: Colors.blue),
         home: MyHomePage(title: "HomePage"));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: new Text(widget.title)),
-      body: new Center(
-          child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-            new Text("You have pushed the button this many times:"),
-            new Text("$_counter", style: Theme.of(context).textTheme.headline4)
-          ])),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: "Increment",
-        child: new Icon(Icons.add),
-      ),
-    );
   }
 }
